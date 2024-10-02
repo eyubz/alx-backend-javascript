bc8fa870a0edbdae330222018c61aaf21a232699
eyubz <eyerusalembezu15@gmail.com>
2024-10-02 09:02:19 +0300

    Files updated.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,12 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return signUpUser(firstName, lastName)
-    .then(() => uploadPhoto(fileName))
-    .then((result) => {
-      console.log(result);
-    })
-    .catch(() => {});
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.value || result.reason,
+  })));
 }
