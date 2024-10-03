a076ff735f862fdb0b097e7fc7b27acd076f83b4
eyubz <eyerusalembezu15@gmail.com>
2024-10-03 18:15:14 +0300

    Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,6 +1,14 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
   }
 }
