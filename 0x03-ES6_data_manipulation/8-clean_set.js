c57e44b5dcbfd8e452681d160f796a68247fa5b1
eyubz <eyerusalembezu15@gmail.com>
2024-10-07 16:28:57 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  const newSet = [];
+  set.forEach((value) => {
+    if (value && value.startsWith(startString)) { newSet.push(value.slice(startString.length)); }
+  });
+  return newSet.join('-');
+}
