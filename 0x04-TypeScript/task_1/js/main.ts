dfd906fea5474716aae479b4df738f55e5e7dead
eyubz <eyerusalembezu15@gmail.com>
2024-10-09 22:40:27 +0300

    Files added.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,11 @@ export interface Teacher {
   [key: string]: any;
 }
 
-const teacher3: Teacher = {
-  firstName: "John",
-  fullTimeEmployee: false,
-  lastName: "Doe",
-  location: "London",
-  contract: false,
-};
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 
-console.log(teacher3);
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+console.log(printTeacher("Eyerusalem", "Teshale"));
