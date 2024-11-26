0f22f562d55088f9ffdf83a101abee8d4b41336e
Bz14 <bezueyerusalem@gmail.com>
2024-11-26 14:23:43 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(filePath) {
   try {
-    const data = fs.readFileSync(filePath, "utf-8");
-    const lines = data.split("\n").filter((line) => line.trim() !== "");
-    if (lines.length <= 1) {
-      console.log("Number of students: 0");
+    const data = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+    if (data.length <= 1) {
+      console.log('Number of students: 0');
       return;
     }
-    const students = lines.slice(1).map((line) => line.split(","));
+    const students = data.slice(1).map((line) => line.split(','));
     console.log(`Number of students: ${students.length}`);
 
     const fields = {};
     for (const student of students) {
       const field = student[3];
       const firstName = student[0];
-
       if (!fields[field]) {
         fields[field] = [];
       }
       fields[field].push(firstName);
     }
-
     for (const [field, names] of Object.entries(fields)) {
-      console.log(field, names);
-      //   console.log(
-      //     `Number of students in ${field}: ${names.length}. List: ${names.join(
-      //       ", "
-      //     )}`
-      //   );
+      const cleanedField = field.trim();
+      const data = `Number of students in ${cleanedField}: ${
+        names.length
+      }. List: ${names.join(', ')}`;
+      console.log(data);
     }
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,7 +8,13 @@ It should be assigned to the variable app and this one must be exported
 HTTP server should listen on port 1245
 It should return plain text
 When the URL path is /, it should display Hello Holberton School! in the page body
-When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
+When the URL path is /students, 
+
+it should display This is the list of our students 
+
+followed by the same content as the file 3-read_file_async.js 
+
+(with and without the database) - the name of the database must be passed as argument of the file
 CSV file can contain empty lines (at the end) - and they are not a valid student!
 Terminal 1:
  */
