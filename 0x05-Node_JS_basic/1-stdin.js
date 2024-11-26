4c1c9cde71770e6640ee50be123255feec94d851
Bz14 <bezueyerusalem@gmail.com>
2024-11-26 13:37:23 +0300

    Files updated.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,9 @@
-/**
- * Prompts the user to enter their name, reads the input from STDIN,
- * and displays a message containing the user's name.
- *
- * When the user ends the program, a closing message is displayed.
- *
- * @example
- * // Output:
- * // Welcome to Holberton School, what is your name?
- * // Alice
- * // Your name is: Alice
- * // This important software is now closing
- */
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
-  const chunk = process.stdin.read();
-
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
