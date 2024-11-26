79c1d3824a21a18112baa4cceb360050372f02a4
Bz14 <bezueyerusalem@gmail.com>
2024-11-26 20:54:22 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,42 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const databaseFile = process.argv[2];
+
+async function countStudents(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    const lines = data.trim().split('\n');
+
+    if (lines.length <= 1) {
+      return 'Number of students: 0';
+    }
+
+    const students = lines.slice(1).map((line) => line.split(','));
+    let result = `Number of students: ${students.length}\n`;
+
+    const fields = {};
+    for (const student of students) {
+      const field = student[3];
+      const firstName = student[0];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    }
+
+    for (const [field, names] of Object.entries(fields)) {
+      const cleanedField = field.trim();
+      result += `Number of students in ${cleanedField}: ${
+        names.length
+      }. List: ${names.join(', ')}\n`;
+    }
+
+    return result.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
@@ -14,6 +49,7 @@ const app = http.createServer(async (req, res) => {
       const studentData = await countStudents(databaseFile);
       res.end(studentData);
     } catch (err) {
+      console.error(err);
       res.end('Cannot load the database');
     }
   } else {
