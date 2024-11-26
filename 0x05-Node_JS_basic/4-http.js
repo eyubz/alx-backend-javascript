885531d3c50a2789a04bf3ff4c56f245751e7638
Bz14 <bezueyerusalem@gmail.com>
2024-11-26 13:06:11 +0300

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,12 @@
 const http = require("http");
+/**
+ * Create a small HTTP server using Node's HTTP module
+ */
 
 const app = http.createServer((_, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello Holberton School!");
 });
-app.listen(1245, "localhost");
+app.listen(1245);
 
 module.exports = app;
