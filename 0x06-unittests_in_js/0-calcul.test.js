e26585c65104f79609e57fe7c3314fe0c3391f64
Bz14 <bezueyerusalem@gmail.com>
2024-11-28 19:32:11 +0300

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,25 @@
+const assert = require("assert");
+
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber", function () {
+  it("should return 4", function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it("should return 5", function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it("should return 4", function () {
+    assert.strictEqual(calculateNumber(0.1, 3.7), 4);
+  });
+  it("should return 5", function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it("should return 6", function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it("should return 1", function () {
+    assert.strictEqual(calculateNumber(0, 0.8), 1);
+  });
+});
