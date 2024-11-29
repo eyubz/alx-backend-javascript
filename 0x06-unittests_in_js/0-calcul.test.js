d93fdcd25accde06a5cacea1380a7b64fc6728b7
Bz14 <bezueyerusalem@gmail.com>
2024-11-29 09:37:11 +0300

    Files updated.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,40 +1,48 @@
 const assert = require("assert");
-
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function () {
-  it("should return 4", function () {
+  it("Should return 4", function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it("should return 5", function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it("Should return -5", function () {
+    assert.strictEqual(calculateNumber(-3, -2), -5);
   });
-  it("should return 4", function () {
-    assert.strictEqual(calculateNumber(0.1, 3.7), 4);
+
+  it("Should return 5", function () {
+    assert.strictEqual(calculateNumber(1.4, 3.7), 5);
   });
-  it("should return 5", function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+
+  it("Should return -5", function () {
+    assert.strictEqual(calculateNumber(-1.4, -3.7), -5);
   });
-  it("should return 6", function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+
+  it("Should return 5", function () {
+    assert.strictEqual(calculateNumber(1.45, 3.79), 5);
   });
-  it("should return 1", function () {
-    assert.strictEqual(calculateNumber(0, 0.8), 1);
+
+  it("Should return NaN", function () {
+    assert.strictEqual(calculateNumber("a", "b"), NaN);
   });
-  it("should return 5", function () {
-    assert.strictEqual(calculateNumber(2, 2.8), 5);
+
+  it("Should return NaN", function () {
+    assert.strictEqual(calculateNumber("goat", "dog"), NaN);
   });
-  it("should return Nan", function () {
-    assert.strictEqual(calculateNumber("hi", "bye"), NaN);
+
+  it("Should return NaN", function () {
+    assert.strictEqual(calculateNumber([1, 2, 3], 5), NaN);
   });
-  it("should return Nan", function () {
-    assert.strictEqual(calculateNumber([], {}), NaN);
+
+  it("Should return NaN", function () {
+    assert.strictEqual(calculateNumber({ a: 24 }, 5), NaN);
   });
-  it("should return Nan", function () {
-    assert.strictEqual(calculateNumber("hi", 2), NaN);
+
+  it("Should return NaN", function () {
+    assert.strictEqual(calculateNumber(), NaN);
   });
-  it("should return Nan", function () {
-    assert.strictEqual(calculateNumber(2, "hi"), NaN);
+
+  it("Should return NaN", function () {
+    assert.strictEqual(calculateNumber(2), NaN);
   });
 });
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -12,4 +12,13 @@ describe("calculateNumber", function () {
   it("should return 2", function () {
     assert.strictEqual(calculateNumber(6, 3, "DIVIDE"), 2);
   });
+  it("should return 4", function () {
+    assert.strictEqual(calculateNumber(1, 3, "SUM"), 4);
+  });
+  it("should return -2", function () {
+    assert.strictEqual(calculateNumber(1, 3, "SUBTRACT"), -2);
+  });
+  it("should return 2", function () {
+    assert.strictEqual(calculateNumber(6, 3, "DIVIDE"), 2);
+  });
 });
