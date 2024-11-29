785a76e0c9bdede2a78fa2ecce3a7beb9de5d345
Bz14 <bezueyerusalem@gmail.com>
2024-11-29 09:42:31 +0300

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,24 +1,27 @@
 const assert = require("assert");
 
-const calculateNumber = require("./0-calcul");
+const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber", function () {
   it("should return 4", function () {
-    assert.strictEqual(calculateNumber(1, 3, "SUM"), 4);
+    assert.strictEqual(calculateNumber("SUM", 1, 3), 4);
   });
   it("should return -2", function () {
-    assert.strictEqual(calculateNumber(1, 3, "SUBTRACT"), -2);
+    assert.strictEqual(calculateNumber("SUBTRACT", 1, 3), -2);
   });
   it("should return 2", function () {
-    assert.strictEqual(calculateNumber(6, 3, "DIVIDE"), 2);
+    assert.strictEqual(calculateNumber("DIVIDE", 6, 3), 2);
   });
-  it("should return 4", function () {
-    assert.strictEqual(calculateNumber(1, 3, "SUM"), 4);
-  });
-  it("should return -2", function () {
-    assert.strictEqual(calculateNumber(1, 3, "SUBTRACT"), -2);
+  it("should return 5", function () {
+    assert.strictEqual(calculateNumber("SUM", 1.4, 3.8), 5);
   });
   it("should return 2", function () {
-    assert.strictEqual(calculateNumber(6, 3, "DIVIDE"), 2);
+    assert.strictEqual(calculateNumber("SUBTRACT", 4.8, 3.4), 2);
+  });
+  it("should return 2", function () {
+    assert.strictEqual(calculateNumber("DIVIDE", 6.2, 3.2), 2);
+  });
+  it("should return Error", function () {
+    assert.strictEqual(calculateNumber("DIVIDE", 6.2, 0), "Error");
   });
 });
