17d7c3ed4cfb653468f1e8ac82140ca31e597d1a
Bz14 <bezueyerusalem@gmail.com>
2024-11-29 10:28:40 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment");
+
+describe("sendPaymentRequestToApi", () => {
+  it("sendPaymentRequestToApi using Utils.caculateNumber", () => {
+    const spyUtils = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(spyUtils.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
+    expect(spyUtils.calculateNumber.calledOnce).to.be.true;
+    spyUtils.calculateNumber.restore();
+  });
+});
