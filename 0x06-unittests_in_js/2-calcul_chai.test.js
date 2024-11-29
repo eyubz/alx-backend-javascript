5e91ac85cf3e39671556a7bd88b6dfc877a8cb2b
Bz14 <bezueyerusalem@gmail.com>
2024-11-29 09:58:47 +0300

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -14,10 +14,6 @@ describe("calculateNumber", () => {
     it("Should test two decimal point number", () => {
       expect(calculateNumber("SUM", 1.35, 5.66)).to.equal(7);
     });
-
-    it("Should test two mixed type", () => {
-      expect(calculateNumber("SUM", "3.5", 5.66)).to.equal(10);
-    });
   });
 });
 
@@ -33,17 +29,9 @@ describe("SUBTRACT", () => {
   it("Should test two decimal point number", () => {
     expect(calculateNumber("SUBTRACT", 5.66, 2.56)).to.equal(3);
   });
-
-  it("Should test two mixed type", () => {
-    expect(calculateNumber("SUBTRACT", "5.66", 2.0)).to.equal(4);
-  });
 });
 
 describe("DIVIDE", () => {
-  it("Should test positive floating number", () => {
-    expect(calculateNumber("DIVIDE", 1.3, 2.5)).to.equal(0.3333333333333333);
-  });
-
   it("Should test positive floating number", () => {
     expect(calculateNumber("DIVIDE", 6.4, 2.4)).to.equal(3.0);
   });
@@ -54,9 +42,5 @@ describe("DIVIDE", () => {
 
   it("Should test for Zero division", () => {
     expect(calculateNumber("DIVIDE", 1.3, 0.4)).to.equal("Error");
-  });
-
-  it("Should test for string type", () => {
-    expect(calculateNumber("DIVIDE", 10.6, "2.4")).to.equal(5.5);
   });
 });
