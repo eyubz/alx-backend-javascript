f1b9b3dc23bcf4b82a4ce1fc8c3fa72dce254796
Bz14 <bezueyerusalem@gmail.com>
2024-11-29 20:34:38 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -13,6 +13,20 @@ app.get("/cart/:id(\\d+)", (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
+app.get("/available_payments", (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
+});
+
 const PORT = 7865;
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
