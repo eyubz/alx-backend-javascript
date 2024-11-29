7488778aaea5199718665922ba7addb046ae7357
Bz14 <bezueyerusalem@gmail.com>
2024-11-29 09:32:25 +0300

    Files updated.

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber", function () {
+  it("should return 4", function () {
+    assert.strictEqual(calculateNumber(1, 3, "SUM"), 4);
+  });
+  it("should return -2", function () {
+    assert.strictEqual(calculateNumber(1, 3, "SUBTRACT"), -2);
+  });
+  it("should return 2", function () {
+    assert.strictEqual(calculateNumber(6, 3, "DIVIDE"), 2);
+  });
+});
